3634fb120c22201eeb7cd10a6bf3c052585caaf3
Tim Gaines <tim@timdoes.com>
2023-04-27 22:02:49 -0700

    create a markdown library for parsing md articles

--- apps/site/pages/articles/[slug]/index.tsx
+++ apps/site/pages/articles/[slug]/index.tsx
@@ -2,16 +2,26 @@ import { readdirSync } from "fs";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { join } from "path";
 import { ParsedUrlQuery } from "querystring";
+import {
+  getParsedFileContentBySlug,
+  renderMarkdown,
+} from '@nx-next-tailwind/markdown';
+import { MDXRemote } from 'next-mdx-remote';
 
 /* eslint-disable-next-line */
 export interface ArticleProps extends ParsedUrlQuery {
-  slug: string;
+  slug?: string;
 }
 
-export function Article({slug}: ArticleProps) {
+export function Article({frontMatter, html}) {
   return (
-    <div>
-      <h1>Visiting, {slug}</h1>
+    <div className="m-6">
+      <article className="prose prose-lg">
+        <h1>{ frontMatter.title }</h1>
+        <div>by { frontMatter.author.name }</div>
+      </article>
+      <hr />
+      <MDXRemote {...html} />
     </div>
   );
 }
@@ -19,14 +29,18 @@ export function Article({slug}: ArticleProps) {
 const POSTS_PATH = join(process.cwd(), '_articles');
 
 export const getStaticProps: GetStaticProps<ArticleProps> = async ({params}: {params: ArticleProps}) => {
+  const articleMarkdownContent = getParsedFileContentBySlug(params.slug, POSTS_PATH);
+  const renderHTML = await renderMarkdown(articleMarkdownContent.content);
+
   return {
     props: {
-      slug: params.slug,
+      frontMatter: articleMarkdownContent.frontMatter,
+      html: renderHTML,
     }
   };
 };
 
-export const getStaticPaths: GetStaticPaths<ArticleProps> = async (context) => {
+export const getStaticPaths: GetStaticPaths<ArticleProps> = async () => {
   const paths = readdirSync(POSTS_PATH)
     .map(path => path.replace(/\.mdx?$/, ''))
     .map(slug => ({ params: { slug }}));
