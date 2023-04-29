6b296ac68e86b36857f8f49cd29f422e2d14ba0c
Tim Gaines <tim@timdoes.com>
2023-04-29 16:15:25 -0700

    setup story for topic-button

--- libs/shared/ui/src/lib/topic-button/topic-button.stories.tsx
+++ libs/shared/ui/src/lib/topic-button/topic-button.stories.tsx
@@ -4,9 +4,24 @@ import { TopicButton } from './topic-button';
 const Story: Meta<typeof TopicButton> = {
   component: TopicButton,
   title: 'TopicButton',
+  argTypes: {
+    onClick: { action: 'onClick executed' }
+  },
 };
+
 export default Story;
 
 export const Primary = {
-  args: {},
+  args: {
+    topicName: 'Nx Workspaces',
+  },
+  parameters: {
+    backgrounds: {
+      values: [
+        { name: 'white', value: '#fff' },
+        { name: 'gray', value: '#999' },
+        { name: 'black', value: '#000' },
+      ],
+    },
+  },
 };
--- libs/shared/ui/src/lib/topic-button/topic-button.tsx
+++ libs/shared/ui/src/lib/topic-button/topic-button.tsx
@@ -1,12 +1,27 @@
-import styles from './topic-button.module.css';
-
 /* eslint-disable-next-line */
-export interface TopicButtonProps {}
+export interface TopicButtonProps {
+  topicName: string;
+  onClick?: (topicName: string) => void,
+}
 
 export function TopicButton(props: TopicButtonProps) {
+
+const onClickHandler = () => {
+  if (props.onClick) {
+    props.onClick(props.topicName);
+  } else {
+    console.warn('No click handler defined on topic button with this topic name');
+  }
+}
+
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to TopicButton!</h1>
+    <div
+      className='bg-white pl-4 rounded-lg shadow flex max-w-md min-w-max hover:shadow-md transition-shadow cursor-pointer'
+      onClick={onClickHandler}
+    >
+      <div className="p-5">
+        <h2 className='font-bold text-4xl'>{props.topicName}</h2>
+      </div>
     </div>
   );
 }
