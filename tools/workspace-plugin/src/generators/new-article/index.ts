99d2f04498929e45bad736412b5d447970a53ed6
Tim Gaines <tim@timdoes.com>
2023-04-29 12:09:23 -0700

    migrate to v16

---
+++ tools/workspace-plugin/src/generators/new-article/index.ts
@@ -0,0 +1,26 @@
+import {
+  Tree,
+  formatFiles,
+  generateFiles,
+  joinPathFragments,
+  names,
+} from '@nx/devkit';
+
+interface NewArticleSchemaOptions {
+  title: string;
+  author: string;
+  excerpt: string;
+  youtubeUid: string;
+}
+
+export default async function (tree: Tree, schema: NewArticleSchemaOptions) {
+  generateFiles(tree, joinPathFragments(__dirname, './files'), './_articles', {
+    title: schema.title,
+    author: schema.author,
+    excerpt: schema.excerpt,
+    youtubeUid: schema.youtubeUid,
+    normalizedTitle: names(schema.title).fileName,
+    creationDate: new Date().toISOString(),
+  });
+  await formatFiles(tree);
+}
