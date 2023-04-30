3ceac2d1e1a22a86847d4dc1c97f1ea5788c7863
Tim Gaines <tim@timdoes.com>
2023-04-30 14:26:07 -0700

    create topics page and import button from shared-ui

--- libs/shared/ui/src/lib/topic-button/topic-button.stories.tsx
+++ libs/shared/ui/src/lib/topic-button/topic-button.stories.tsx
@@ -4,16 +4,15 @@ import { TopicButton } from './topic-button';
 const Story: Meta<typeof TopicButton> = {
   component: TopicButton,
   title: 'TopicButton',
-  argTypes: {
-    onClick: { action: 'onClick executed' }
-  },
 };
 
 export default Story;
 
 export const Primary = {
   args: {
-    topicName: 'Nx Workspaces',
+    topicName: 'GitHub',
+    icon: 'FaGithub',
+    href: 'https://github.com/timdoes',
   },
   parameters: {
     backgrounds: {
